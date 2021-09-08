1ec073646a43a934c6b572f6f861932260b0a32e
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2021-09-08 11:12:30 +0200

    Vi tränade cw och special tecken

---
+++ Progr 1 test/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Progr_1_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Mitt namn är Gustav Berg");
+
+            Console.WriteLine("It is a long established fact that a\n"+
+             "reader will be distracted by the readable content of a\n"+
+             "page when looking at its layout. The point of using Lorem Ip\n");
+
+            Console.WriteLine("Mitt spelnick är \"Scrubsy\"");
+        }
+    }
+}
