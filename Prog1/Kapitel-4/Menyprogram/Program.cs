e40e652791ec4029445701c1e6f0b07d74e7a857
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2021-10-15 15:23:59 +0200

    sten sax påse

---
+++ Prog1/Kapitel-4/Menyprogram/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Menyprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enkelt menyprogram!");
+
+            //Skapa en slumpgenerator
+            Random tärning = new Random();
+
+            //Programloopen
+            while (true)
+            {
+                //Slumpa fram ett tal
+                int slumptal = tärning.Next(1, 4);
+
+                 Console.WriteLine("Välj ett av följande alternativ ");
+                 Console.WriteLine("Väljer du sten? (1)");
+                 Console.WriteLine("Väljer du sax? (2)");
+                 Console.WriteLine("Väljer du påse? (3)");
+                 Console.WriteLine("Väljer du avsluta? (4)");
+                 string valString = Console.ReadLine();
+
+                 //Vill användaren avsluta?
+                 if (valString == "4")
+                 {
+                     break;
+                 }
+
+                 int val = int.Parse(valString);
+
+                 //Om
+                 if ((slumptal == 1 && valString== "1"))
+                 {
+                     Console.WriteLine("Lika!");
+                 }
+                 if (slumptal == 1 && valString == "2")
+                 {
+                     Console.WriteLine("Du förlorar!");
+                 }
+                 if (slumptal == 1 && valString == "3")
+                 {
+                     Console.WriteLine("Du vinner!");
+                 }
+                 //Om
+                 if ((slumptal == 2 && valString== "1"))
+                 {
+                     Console.WriteLine("Du vinner!");
+                 }
+                 if (slumptal == 2 && valString == "2")
+                 {
+                     Console.WriteLine("Lika!");
+                 }
+                 if (slumptal == 2 && valString == "3")
+                 {
+                     Console.WriteLine("Du förlorar!");
+                 }
+                 //Om
+                 if ((slumptal == 3 && valString== "1"))
+                 {
+                     Console.WriteLine("Du förlorar!");
+                 }
+                 if (slumptal == 3 && valString == "2")
+                 {
+                     Console.WriteLine("Du vinner!");
+                 }
+                 if (slumptal == 3 && valString == "3")
+                 {
+                     Console.WriteLine("Lika!");
+                 }
+
+
+                 //Är det rätt svar
+                 if (val == slumptal)
+                 {
+                    Console.WriteLine("Du vinner!");
+                 }
+                 else
+                 {
+                    Console.WriteLine("Du förlorar!");
+                 }
+            }
+        }
+    }
+}
---
+++ Prog1/Prover/Prov-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Prov_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Berätta vad programmet gör
+            Console.WriteLine("Uträkning av nettolön\n---------------------");
+
+            //Fråga efter namn
+            Console.Write("Vad heter du? ");
+            string namn = Console.ReadLine();
+
+            //Fråga efter bruttolön
+            System.Console.WriteLine("Ange din bruttolön i kronor");
+            int bruttolön = int.Parse(Console.ReadLine());
+
+            //Fråga efter skattesats
+            System.Console.WriteLine("Ange din skattesats i kronor");
+            int skattesats = int.Parse(Console.ReadLine());
+
+            //Kolla att villkoren är uppfyllda
+            if (bruttolön > 10000 && bruttolön <= 45000)
+            {
+                //Fråga efter skattesats
+                Console.Write("Ange din skattesats i kronor: ");
+
+                //Kolla att villkoren är uppfullda för skattesats
+                if (skattesats >= 10 && skattesats <=45)
+                {
+                    //Räkna ut nettolön
+                    
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bruttolön måste vara mellan 10000 och 45000");
+            }
+                        
+            
+            //Räkna ut nettolön
+            int nettolön = bruttolön * (100 - skattesats) / 100;
+            Console.WriteLine($"{namn}, din lön efter skatt är {nettolön}");
+
+
+        }
+    }
+}
