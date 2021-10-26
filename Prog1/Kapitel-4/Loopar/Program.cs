8c367407f825eed58f69811271ba62a622aacbb9
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2021-10-26 09:57:35 +0200

    krypto

---
+++ Prog1/Kapitel-4/Loopar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Loopar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skriv ut 1, 2, 3, 4, 5
+            for (int i = 1; i < 6; i++)
+            {
+              Console.WriteLine(i);
+            }
+            //Skriv ut 9, 8, 7, 6, 5, 4, 3, 2, 1
+            for (int i = 9; i > 0; i--)
+            {
+            Console.WriteLine(i);
+
+            }
+            //Summera alla siffror 1-10: 1+2+3+4+5+6+7+8+9+10
+            int summa = 0;
+            for (int i = 0; i < 11; i++)
+            {
+                summa += i;
+            }
+            Console.WriteLine($"Summan är {summa}");
+
+            string namn = "Mohammed";
+
+            //Skriv ut andra bokstaven
+            Console.WriteLine(namn[0]);   //M
+            Console.WriteLine(namn[1]);   //o
+            Console.WriteLine(namn[3]);   //a
+            Console.WriteLine(namn[4]);   //m
+
+            //Skriv ut mohammed baklänges
+            for (int i = 7; i > -1; i--)
+            {
+                Console.WriteLine(namn[i]);
+            }
+        }
+    }
+}
---
+++ Prog1/Kapitel-4/Uppgift 4.6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Uppgift_4._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string meddelande = "Hallå allesammans";
+
+            //skriv ut hela stringen
+            Console.WriteLine(meddelande);
+
+            Console.WriteLine(meddelande[0]);
+
+            Console.WriteLine(meddelande[17]);
+
+            //Skriv ut alla tecken baklänges
+            for (int i = 17; i >= 0; i--)
+            {
+                Console.WriteLine($"index {i} {meddelande[i]}");
+            }
+        }
+    }
+}
