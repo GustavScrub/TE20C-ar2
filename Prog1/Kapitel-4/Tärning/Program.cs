c027a3e9aa049b2910d18895509db475aaf6ed49
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2021-11-24 10:21:58 +0100

    array

---
+++ Prog1/Kapitel-4/Tärning/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tärning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Skapa ett program som ska fungera som en tärningssimulator
+            //När användaren startar programmet ska hen få säga hur många tärningar hen vill kasta och hur många sidor varje tärning ska ha
+            //Simulera därefter det antal tärningskast som användaren vill göra med det antal sidor som användaren valde
+            //Skriv ut värdet av varje tärningskast
+
+        }
+    }
+}
---
+++ Prog1/Kapitel-5/TP-frågor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TP_frågor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Slumpa fram en fråga");
+
+            //Namnge korten (array)
+            string[] korten = {
+                "När uppfördes Verdi?",
+                "Vem vann fotbolls-Em 1984?",
+                "Vem skrev Frankenstein?",
+                "Vad är 60+9?",
+                "När startade ww2?",
+                "Vem är president i USA 2021?"
+                };
+
+            //Kasta tärningen
+            Random tärning = new Random();
+            int slumptal = tärning.Next(0, 6); // 0 till 5
+
+            //Skriver utslaget
+            Console.WriteLine($"Tärningen slog {slumptal}");
+
+            //Skriv ut fråga 2
+            Console.WriteLine($"Frågan: {korten[slumptal]}");
+        }
+    }
+}
