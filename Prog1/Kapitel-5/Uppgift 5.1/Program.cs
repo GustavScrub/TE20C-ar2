9947220a1a84fcc0cd6914b41e8d5c54d2f3d5c6
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2021-12-07 10:17:38 +0100

    sync

---
+++ Prog1/Kapitel-5/Uppgift 5.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Uppgift 5.1");
+            
+            // Skapa en array för 3 öar
+            string[] städer = new string[3];
+ 
+            // Första platsen (0)
+            städer[0] = "Stockholm";
+            städer[1] = "Malmö";
+            // Låt användaren skriva den tredje staden
+            Console.WriteLine($"Skriv in en till svensk stad förutom {städer[0]} och {städer[1]}");
+            städer[2] = Console.ReadLine();
+
+            // Resultatet
+            Console.WriteLine("Här är 3 svenska städer:");
+            Console.WriteLine($"{städer[0]}, {städer[1]} och {städer[2]}");
+        }
+    }
+}
---
+++ Prog1/Kapitel-6/TryParse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Gissa ett tal");
+
+            //Slumpgenerator
+            Random generator = new Random();
+
+            //Slumpa fram ett tal mellan
+            int slumptal = generator.Next(1, 101); //1-100
+            int slumptal2 = generator.Next(1, 11); //1-10
+
+            //Be spelaren att gissa ett tal
+            Console.WriteLine("Gissa ett tal (1-100): ");
+            //int gissning = int.Parse(Console.ReadLine());
+
+            //Försök överästta det inmatade till ett tal
+            int.TryParse(gissning, out gissningInt) ;
+
+            //Var gissnignen korrekt
+            if (gissning == slumptal)
+            {
+                Console.WriteLine("Du gissade rätt!");
+            }
+            else
+            {
+                Console.WriteLine("Du gissade fel...");
+            }
+        }
+    }
+}
