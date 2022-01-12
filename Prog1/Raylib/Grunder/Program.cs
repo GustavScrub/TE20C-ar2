ef5fed63d53ceec0da7654fc588f74dcb44f39a7
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2022-01-12 11:04:18 +0100

    Intro till raylib

---
+++ Prog1/Raylib/Grunder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Grunder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there Raylib!");
+
+            // Starta ett fönster
+            Raylib.InitWindow(800, 600, "Ett Raylib fönster");
+
+            // Ställ in fps
+            Raylib.SetTargetFPS(60);
+
+            // En egen färg
+            Color GoldenRod = new Color(218, 165, 32, 170);
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                 // Börja rita
+                 Raylib.BeginDrawing();
+
+                 // Tömma ritytan
+                 Raylib.ClearBackground(Color.DARKBLUE);
+
+                 // Rita en rektangel
+                 Raylib.DrawRectangle(1, 250, 800, 80, Color.GOLD);
+                 Raylib.DrawRectangle(200, 0, 100, 600, Color.GOLD);
+
+                 // Rita en cirkel
+                 Raylib.DrawCircle(400, 300, 100, GoldenRod);
+
+                 // Rita text
+                 Raylib.DrawText("Det här är en cirkel", 100, 500, 50, Color.RED);
+
+                 // Sluta rita
+                 Raylib.EndDrawing();
+            }
+        }
+    }
+}
---
+++ Prog1/Raylib/RAF/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Raylib_cs;
+
+namespace RAF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello RAF!");
+
+            Raylib.InitWindow(800, 600, "Into the window");
+
+            Raylib.SetTargetFPS(60);
+
+            Console.WriteLine("Hann inte med det sista, men det är basic grejer jag har koll på.");
+
+        }
+    }
+}
