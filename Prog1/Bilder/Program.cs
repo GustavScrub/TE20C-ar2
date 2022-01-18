0e7b1e4b47b6abfd460379eb36803160508fc9ff
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2022-01-18 11:04:40 +0100

    tränade på bilder och input

---
+++ Prog1/Bilder/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Ladda in bilden
+            Texture2D månen = Raylib.LoadTexture(@"./resur");
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                 // Börja rita
+                 Raylib.BeginDrawng();
+                 
+                 // Töm ritytan
+                 Raylib.ClearBackground
+            }
+        }
+    }
+}
---
+++ Prog1/Raylib/Input/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Raylib_cs;
+
+namespace Bilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Starta grafikmotorn
+            Raylib.InitWindow(800, 600, "Mitt Raylib fönster");
+
+            // Bestäm skärmuppdatering
+            Raylib.SetTargetFPS(60);
+
+            // Skapa en spelare av en rektangel
+            Rectangle spelare = new Rectangle(100, 100, 50, 50);
+
+             // Skapa en fiende
+            Rectangle fiende = new Rectangle(100, 200, 50, 50);
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                /* Rita ut grafiken */
+                 // Börja rita
+                 Raylib.BeginDrawing();
+                 
+                 // Töm ritytan
+                 Raylib.ClearBackground(Color.DARKBLUE);
+
+                 // Rita ut spelaren
+                 Raylib.DrawRectangleRec(spelare, Color.RED);
+
+                 // Rita ut på fönstret
+                 Raylib.EndDrawing();
+
+                 /* Intereaktion med användaren */
+                 // Lyssna på tangenter
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                 {
+                     spelare.x += 5;
+                 }
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                 {
+                     spelare.x -= 5;
+                 }
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                 {
+                     spelare.y -= 5;
+                 }
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                 {
+                     spelare.y += 5;
+                 }
+
+                 // Rita ut spelaren
+                 Raylib.DrawRectangleRec(spelare, Color.GREEN);
+
+                 // Rita ut på fönstret
+                 Raylib.EndDrawing();
+
+                 /* Intereaktion med användaren */
+                 // Lyssna på tangenter
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_D))
+                 {
+                     spelare.x += 5;
+                 }
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_A))
+                 {
+                     spelare.x -= 5;
+                 }
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_W))
+                 {
+                     spelare.y -= 5;
+                 }
+                 if (Raylib.IsKeyDown(KeyboardKey.KEY_S))
+                 {
+                     spelare.y += 5;
+                 }
+            }
+        }
+    }
+}
