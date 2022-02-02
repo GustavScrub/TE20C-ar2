00e705051f5edb58853b7eeecb87ecbd8ba545c5
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2022-02-02 11:07:38 +0100

    fall, kollisioner, tangenter

---
+++ Prog1/Raylib/SpaceDodgers/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Raylib_cs;
+
+namespace SpaceDodgers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialisering
+            //--------------------------------------------------------------------------------------
+            const int fönsterB = 800;
+            const int fönsterH = 600;
+
+            Raylib.InitWindow(fönsterB, fönsterH, "Dodgers");
+            Raylib.SetTargetFPS(60);
+
+            // Game state variabler
+            int poäng = 0;
+            int liv = 3;
+            float tid = 0;
+
+            // TODO: Infoga variabler och objekt här
+            Random generator = new Random();
+            Rectangle mynt1 = new Rectangle(400, 0, 50, 50);
+            Rectangle mynt2 = new Rectangle(400, 0, 50, 50);
+            Rectangle monster = new Rectangle(400, 0, 50, 50);
+            Rectangle spelare = new Rectangle(100, fönsterH - 50, 100, 10);
+            //--------------------------------------------------------------------------------------
+
+            // Animationsloopen
+            while (!Raylib.WindowShouldClose())
+            {
+                // Updatering
+                //----------------------------------------------------------------------------------
+                // Räkna upp tiden
+                tid += Raylib.GetFrameTime();
+
+                // Hastigheter och nedre gräns
+                mynt1.y += 1;
+                if (mynt1.y > fönsterH)
+                {
+                    mynt1.y = -100;
+                    mynt1.x = generator.Next(0, fönsterB);
+                }
+
+                mynt2.y += 1;
+                if (mynt2.y > fönsterH)
+                {
+                    mynt2.y = -100;
+                    mynt2.x = generator.Next(0, fönsterB);
+                }
+                monster.y += 1;
+                if (monster.y > fönsterH)
+                {
+                    monster.y = -100;
+                    monster.x = generator.Next(0, fönsterB);
+                }
+                // Lyssna på tangenter
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    spelare.x -= 4;
+                }
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    spelare.x += 4;
+                }
+
+                // Om spelaeren träffar ett mynt
+                if (Raylib.CheckCollisionRecs(spelare, mynt1))
+                {
+                    mynt1.y = 0;
+                    mynt1.x = generator.Next(0, fönsterB);
+                    poäng += 5;
+                }
+                if (Raylib.CheckCollisionRecs(spelare, mynt2))
+                {
+                    mynt2.y = 0;
+                    mynt2.x = generator.Next(0, fönsterB);
+                    poäng += 5;
+                }
+
+                if (Raylib.CheckCollisionRecs(spelare, monster))
+                {
+                    monster.y = 0;
+                    monster.x = generator.Next(0, fönsterB);
+                    liv--;
+                }
+                //----------------------------------------------------------------------------------
+
+                // Rita
+                //----------------------------------------------------------------------------------
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKBLUE);
+
+                Raylib.DrawRectangleRec(mynt1, Color.GOLD);
+                Raylib.DrawRectangleRec(mynt2, Color.GOLD);
+                Raylib.DrawRectangleRec(mynt2, Color.RED);
+                Raylib.DrawRectangleRec(spelare, Color.WHITE);
+                Raylib.DrawText($"Poäng:{poäng} Liv:{liv} Tid:{(int)tid}", 10, 10, 20, Color.YELLOW);
+
+                Raylib.EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+        }
+    }
+}
+
