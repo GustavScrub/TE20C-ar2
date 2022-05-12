e44b7aecb550c683ffd0c62edff33e09185be66a
Gustav Berg <berg.gustav@elev.ga.ntig.se>
2022-05-12 21:44:33 +0200

    why not

---
+++ Prog1/Randomness/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Randomness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            while (true)
+            {
+                Console.WriteLine("Välj mellan motståndare 0-2: ");
+                string[] motståndarna = new string[3]; // motståndarna har positionerna 0, 1, 2
+                motståndarna[0] = "Muzan Kibutsuji (0)";
+                motståndarna[1] = "Ryomen Sukuna (1)";
+                motståndarna[2] = "Eren Yeager (2)";
+
+                foreach (var motståndare in motståndarna)
+                {
+                    Console.WriteLine(motståndare);
+                }
+
+                int spelarSvar = 0;
+                spelarSvar = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Du har valt {motståndarna[spelarSvar]}");
+                string type = Console.ReadLine();
+                
+                while (type == "w")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
